84f61306094d8b9a7fae414d1d911b0e1c598a4f
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-11-10 19:33:09 -0300

    123

--- src/main/java/br/insper/projeto/historico/repository/HistoricoRepository.java
+++ src/main/java/br/insper/projeto/historico/repository/HistoricoRepository.java
@@ -2,12 +2,13 @@ package br.insper.projeto.historico.repository;
 
 import br.insper.projeto.historico.model.Historico;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 
 import java.util.List;
 
 @Repository
-public interface HistoricoRepository extends JpaRepository<Historico, Integer> {
+public interface HistoricoRepository extends MongoRepository<Historico, Integer> {
     List<Historico> findByEmail(String email);
 
 }

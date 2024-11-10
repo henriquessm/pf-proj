ace1eb39b15d78301653e42909aab6c9b7c8bdaf
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-11-10 19:50:29 -0300

    123

--- src/main/java/br/insper/projeto/historico/model/Historico.java
+++ src/main/java/br/insper/projeto/historico/model/Historico.java
@@ -19,6 +19,19 @@ public class Historico {
 
     private String email;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
